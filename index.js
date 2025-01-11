a195c7f7bf54ee3b4e1738a1dd3e52a9415e4869
ABDUL LATHEEF <latheef.mitco@gmail.com>
2025-01-11 10:32:28 +0530

    port updated

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.use((err, req, res, next) => {
 
 
 
-const PORT = process.env.PORT || 5056
+const PORT = process.env.PORT || 5001
 app.listen(PORT, () => console.log(`App is listening on http://localhost:${PORT}`));
 
 
