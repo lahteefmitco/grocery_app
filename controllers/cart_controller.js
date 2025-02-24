c4068c977dcd315df780eefa7fa69f794a872c06
ABDUL LATHEEF <latheef.mitco@gmail.com>
2025-02-24 13:33:10 +0530

    Get user by id is updated

--- controllers/cart_controller.js
+++ controllers/cart_controller.js
@@ -356,7 +356,9 @@ const getCartsByUserId = async (req, res, next) => {
         });
 
         if (carts.length === 0) {
-            return next(createError.NotFound("No orders found for this user"));
+            console.log("-----------");
+            
+            return res.send([]);
         }
 
         carts.forEach(cart => {
